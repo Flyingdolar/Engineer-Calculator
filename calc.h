38c40ea279268efcc335e104f4a22fc5c5caffcf
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-13 16:20:27 +0000

    feat[calc.h]: Add new Struct & Global Variables

--- calc.h
+++ calc.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <math.h>
+
 #include <iostream>
 #include <string>
 #include <variant>
@@ -6,9 +8,48 @@
 
 using namespace std;
 
+enum class Operator {
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    EXP,
+    LOG,
+    SQRT,
+    SIN,
+    COS,
+    TAN,
+    LBR,
+    RBR,
+    LSP,
+    RSP
+};
+
+enum class Constant {
+    PI,
+    E
+};
+
+const pair<string, int> opStr[] = {
+    {"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}, {"^", 3}, {"log", 3}, {"sqrt", 3}, {"sin", 3}, {"cos", 3}, {"tan", 3}, {"(", 0}, {")", 0}, {"[", 0}, {"]", 0}};
+
+const pair<string, float> constStr[] = {
+    {"pi", 3.1415926535897932384626433832795f}, {"e", 2.7182818284590452353602874713527f}};
+
 struct Variable {
     string name;
     variant<int, float> value;
+};
+
+struct OpElement {
+    enum class Type {
+        OPERATOR,
+        VARIABLE,
+        INT,
+        FLOAT
+    } type;
+    Type type;
+    variant<Operator, Variable, int, float> value;
 };
 
 int setVariable(vector<string> strList, vector<Variable>& varList);
