a7fdb71f7897a8388732db4c216fb544de9fa72b
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-13 16:17:59 +0000

    feat[calc.cpp]: Initial & set the variables.

--- calc.cpp
+++ calc.cpp
@@ -9,23 +9,22 @@
 using namespace std;
 
 regex pattern("/-?\\d+(\\.\\d+)?|[a-z]+|[=\\+\\-\\*\\/\\(\\)]/gm");
-regex comment("\\/\\*.*\\*\\/|\\/\\/.*");
-
-int rmComment(string &str) {
-    string::size_type start, end;
-    start = str.find("//");
-    if (start != string::npos)
-        str.erase(start);
-    return 0;
-}
+regex comment("\\/\\/.*");
 
 int setVariable(vector<string> strList, vector<Variable> &varList) {
-    string::size_type start, end;
-
+    Variable var = {"", 0};
     for (auto &str : strList) {
-        rmComment(str);
-        if (str.empty()) continue;
-        vector<string> wordList;
+        str = regex_replace(str, comment, "");
+        sregex_iterator it(str.begin(), str.end(), pattern), end_it;
+        for (; it != end_it; ++it) {
+            if (it->str() == "int" || it->str() == "float") {
+                if (it->str() == "int") var.value = 0;
+                if (it->str() == "float") var.value = 0.0f;
+                continue;
+            }
+            var.name = it->str();
+            varList.push_back(var);
+        }
     }
     return 0;
 }
